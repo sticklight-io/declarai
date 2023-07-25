6922aedf47da0d2989fdee5155d21d624840a0eb
matankleyman <matan.k@pecan.ai>
2023-07-25 18:09:01 +0300

    remove key

--- src/declarai/llm/openai_llm/config.py
+++ src/declarai/llm/openai_llm/config.py
@@ -2,4 +2,4 @@ from pydantic import BaseSettings
 
 
 class OpenAIConfig(BaseSettings):
-    OPENAI_API_KEY: str = "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"
+    OPENAI_API_KEY: str = ""
