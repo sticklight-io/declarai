c0221ce73b7eff6c2f29c6ffb188b4e429a1001c
Aviv Almashanu <avivex1000@gmail.com>
2023-07-29 20:16:27 +0300

    move tests to seperate dir

--- evals/generation/__init__.py
+++ evals/generation/__init__.py
@@ -1,8 +1,6 @@
+import importlib
+
 from .structured_open_ended import structured_open_ended, structured_open_ended_kwargs
-from .structured_strict_complex import (
-    structured_strict_complex,
-    structured_strict_complex_kwargs,
-)
 from .unstructured_long_form import (
     unstructured_long_form,
     unstructured_long_form_kwargs,
@@ -11,3 +9,10 @@ from .unstructured_short_form import (
     unstructured_short_form,
     unstructured_short_form_kwargs,
 )
+
+
+if importlib.util.find_spec("pydantic"):
+    from .structured_strict_complex import (
+        structured_strict_complex,
+        structured_strict_complex_kwargs,
+    )
