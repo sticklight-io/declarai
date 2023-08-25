485f2790c34fa073a0d0de5a49186b6d9347407f
Shobhit Kumar <116124866+shobhit9957@users.noreply.github.com>
2023-08-25 18:39:46 +0530

    fix: #96 Fix formatting issues using pre-commit hooks
    Signed-off-by: Shobhit Kumar <116124866+shobhit9957@users.noreply.github.com>

--- src/declarai/operators/openai_operators/settings.py
+++ src/declarai/operators/openai_operators/settings.py
@@ -5,7 +5,9 @@ import os
 
 from declarai.core.core_settings import DECLARAI_PREFIX
 
-OPENAI_API_KEY: str = os.getenv(f"{DECLARAI_PREFIX}_OPENAI_API_KEY", os.getenv("OPENAI_API_KEY", ""))  # pylint: disable=E1101
+OPENAI_API_KEY: str = os.getenv(
+    f"{DECLARAI_PREFIX}_OPENAI_API_KEY", os.getenv("OPENAI_API_KEY", "")
+)  # pylint: disable=E1101
 "API key for openai provider."
 OPENAI_MODEL: str = os.getenv(
     f"{DECLARAI_PREFIX}_OPENAI_MODEL", "gpt-3.5-turbo"
